1278c796d1d6a557e9f5574aa556c80ef6cb69a6
costar011 <dnaer456@gmail.com>
2023-07-14 08:34:13 +0900

    js

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const tab = document.querySelectorAll(".tab i");
 
 tab.forEach((e) => {
     e.addEventListener("click", (_) => {
-        tab
-    })
-})
+        tab.forEach((e)=> e.classList.remove("active"));
+        content.forEach((e)=> e.classList.remove("active"));
+
+        let currentTab = _.target.getAttribute("data-tab");
+        let currenContent = document.querySelector(
+            `.content[data-content="${currentTab}"]`
+        );
+
+        currenContent.classList.add("active");
+        pointer.style.transform = `translateX(${58 * currentTab}px)`;
+        _.target.classList.add("active");
+    });
+});
