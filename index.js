1a03a9fd5d0731defa18d72ac605d13fb924e62f
costar011 <dnaer456@gmail.com>
2023-07-13 21:04:29 +0900

    js

---
+++ index.js
@@ -0,0 +1,9 @@
+const pointer = document.querySelector(".pointer");
+const content = document.querySelectorAll(".content");
+const tab = document.querySelectorAll(".tab i");
+
+tab.forEach((e) => {
+    e.addEventListener("click", (_) => {
+        tab
+    })
+})
